c20f4049cadfae830c8703473173b7e5320666dd
joshade7 <63887881+joshade7@users.noreply.github.com>
2022-09-22 16:02:27 +0100

    Add files via upload

---
+++ scripts/navbar.js
@@ -0,0 +1,31 @@
+
+let mynav =
+`
+<details>
+<summary>
+  <img src="images/menu-lined.svg" width="40" height="40" alt="" />
+</summary>
+<div id="myMenu">
+  <a href="index.html">Home</a> |
+  <a href="about.html">About us</a> |<a
+    href="https://creativewords.co.uk/resources"
+    >Resources</a
+  >|
+  <a href="https://imageresizer.com/">Imagereizer</a>
+  |<a href="https://unsplash.com/">Unsplash</a> |<a href=""></a> |
+  <a href="contact.html">Contact us</a> |
+  <a href="products.html">Products</a>
+</div>
+</details>
+`;
+
+// document.querySelector("nav").innerHTML = mynav;
+document.getElementById("majornav").innerHTML = mynav;
+
+
+let myFooter= 
+`
+101 Downing street| Admin@cps.co.uk | tel. 02028561456 | &copy; 2022 
+
+`;
+document.getElementById('majorfooter').innerHTML = myFooter;
